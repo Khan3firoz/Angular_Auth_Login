39b12d7321b4eed78a50e766dac002d6dbc7453e
Firoz <khan3firoz@gmail.com>
2021-03-13 18:55:07 +0530

    added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { EventsComponent } from './events/events.component';
 import { SpecialEventsComponent } from './special-events/special-events.component';
 import { AuthService } from './auth.service';
 import { EventService } from './event.service';
@@ -25,7 +24,6 @@ import { NavbarComponent } from './navbar/navbar.component';
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    EventsComponent,
     SpecialEventsComponent,
     SyllabusComponent,
     NavbarComponent,
